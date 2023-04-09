bff6dc6eb8aa6bd2172d95210e95ca7330dc93d3
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-09 11:17:54 -0300

    ...

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./Calendario.module.scss";
 import ptBR from "./localizacao/ptBR.json";
-import Kalend, { CalendarView } from "kalend";
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from "kalend";
 import "kalend/dist/styles/index.css";
-import { useRecoilValue} from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { listaDeEventosState } from "../../state/atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 interface IKalendEvento {
     id?: number;
@@ -15,9 +16,9 @@ interface IKalendEvento {
 }
 
 const Calendario: React.FC = () => {
-    
     const eventosKalend = new Map<string, IKalendEvento[]>();
     const eventos = useRecoilValue(listaDeEventosState);
+    const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
 
     eventos.forEach((evento) => {
         const chave = evento.inicio.toISOString().slice(0, 10);
@@ -32,9 +33,27 @@ const Calendario: React.FC = () => {
             color: "blue",
         });
     });
+
+    const onEventDragFinish: OnEventDragFinish = (KalendEventoInalterado: CalendarEvent, KalendEventoAtualizado: CalendarEvent) => {
+        const evento = eventos.find((evento) => evento.descricao === KalendEventoAtualizado.summary);
+        if (evento) {
+            const eventoAtualizado = {
+                ...evento,
+            };
+            eventoAtualizado.inicio = new Date(KalendEventoAtualizado.startAt);
+            eventoAtualizado.fim = new Date(KalendEventoAtualizado.endAt);
+
+            setListaDeEventos(listaAntiga => {
+                const indice = listaAntiga.findIndex(evt => evt.id === evento.id);
+          
+                return [...listaAntiga.slice(0, indice), eventoAtualizado, ...listaAntiga.slice(indice + 1)]
+              })
+        }
+    };
+
     return (
         <div className={style.Container}>
-            <Kalend events={Object.fromEntries(eventosKalend)} initialDate={new Date().toISOString()} hourHeight={60} initialView={CalendarView.WEEK} timeFormat={"24"} weekDayStart={"Monday"} calendarIDsHidden={["work"]} language={"customLanguage"} customLanguage={ptBR} />
+            <Kalend events={Object.fromEntries(eventosKalend)} initialDate={new Date().toISOString()} hourHeight={60} initialView={CalendarView.WEEK} timeFormat={"24"} weekDayStart={"Monday"} calendarIDsHidden={["work"]} language={"customLanguage"} customLanguage={ptBR} onEventDragFinish={onEventDragFinish}/>
         </div>
     );
 };
